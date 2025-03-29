6ccc70a8bb14fd6d967fb0ea17213595883f5c76
Kamikaze <timo1322@gmail.com>
2025-03-29 02:50:50 +0100

    Update lib
    bump version

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -2,7 +2,7 @@
 // @name        	Kamikaze' Script Utils
 // @namespace    	https://greasyfork.org/users/928242
 // @description  	Custom Functions for Kamikaze's Scripts
-// @version    		1.1.10
+// @version    		1.1.11
 // @author       	Kamikaze (https://github.com/Kamiikaze)
 // @license     	MIT
 // ==/UserScript==
